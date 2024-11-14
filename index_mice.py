c39e374ba4522906fc2d689382856c79394f8754
Andrew Webb <andrew.webb@tuebingen.mpg.de>
2024-11-14 10:59:06 +0100

    Example restructing of code into functions

--- index_mice.py
+++ index_mice.py
@@ -1,68 +1,92 @@
 """
     Script for collection info about the MCMC chains of all subjects.
 """
+import argparse
 import os
 import json
 import re
 import numpy as np
 
-prebias_subinfo = {}
-bias_subinfo = {}
 
-fitvar = '0.04'
+def index_mice(file_prefix, data_folder, fitvar):
+    prebias_subinfo = {}
+    bias_subinfo = {}
+    verificator = lambda x: '/summarised_sessions/0_25/' in x['file_name'] and \
+                            x['fit_variance'] == 0.04 and \
+                            ('dur' not in x or x['dur'] == 'yes') and \
+                            np.array_equal(x['dur_params']['r_support'], np.arange(5, 705)) and \
+                            x['n_states'] == 15 and \
+                            x['gamma_a_0'] == 0.01 and \
+                            x['gamma_b_0'] == 100 and \
+                            x['alpha_a_0'] == 0.01 and \
+                            x['alpha_b_0'] == 100
 
-file_prefix = ['./', '/usr/src/app/'][0]
-data_folder = "dynamic_GLMiHMM_crossvals"
+    print(file_prefix, data_folder, fitvar)
 
-verificator = lambda x: '/summarised_sessions/0_25/' in x['file_name'] and x['fit_variance'] == 0.04 and ('dur' not in x or x['dur'] == 'yes') and np.array_equal(x['dur_params']['r_support'], np.arange(5, 705)) and x['n_states'] == 15 and x['gamma_a_0'] == 0.01 and x['gamma_b_0'] == 100 and x['alpha_a_0'] == 0.01 and x['alpha_b_0'] == 100
+    # look through all files
+    target_directory = os.path.join(file_prefix, data_folder)
+    for filename in os.listdir(target_directory):
+        if not filename.endswith('.p'):
+            continue
 
-# look through all files
-for filename in os.listdir(file_prefix + data_folder + "/"):
-    if not filename.endswith('.p'):
-        continue
+        # extract information about the chain
+        regexp = re.compile(r'((\w|-)+)_fittype_(\w+)_var_{}_(\d+)_(\d+)_(\d+)'.format(fitvar))
+        result = regexp.search(filename)
+        if result is None:
+            continue
+        subject = result.group(1)
+        fit_type = result.group(3)
+        seed = result.group(4)
+        fit_num = result.group(5)
+        chain_num = result.group(6)
 
-    # extract information about the chain
-    regexp = re.compile(r'((\w|-)+)_fittype_(\w+)_var_{}_(\d+)_(\d+)_(\d+)'.format(fitvar))
-    result = regexp.search(filename)
-    if result is None:
-        continue
-    subject = result.group(1)
-    fit_type = result.group(3)
-    seed = result.group(4)
-    fit_num = result.group(5)
-    chain_num = result.group(6)
+        # KS014_0.04_102_522.json
+        infos_filename = os.path.join(file_prefix, data_folder, "infos", f"{subject}_{fitvar}_{seed}_{fit_num}.json")
+        infos = json.load(open(infos_filename))
+        if not verificator(infos):
+            continue
 
-    # KS014_0.04_102_522.json
-    infos = json.load(open(file_prefix + data_folder + "/infos/" + "{}_{}_{}_{}.json".format(subject, fitvar, seed, fit_num), 'r'))
-    if not verificator(infos):
-        continue
+        # add info to correct dictionary
+        if fit_type == 'prebias':
+            local_dict = prebias_subinfo
+        elif fit_type == 'bias':
+            local_dict = bias_subinfo
+        if subject not in local_dict:
+            local_dict[subject] = {"seeds": [], "fit_nums": [], "chain_num": 0}
+        if int(chain_num) == 0:  # if this is the first file of that chain, save some info
+            local_dict[subject]["seeds"].append(seed)
+            local_dict[subject]["fit_nums"].append(fit_num)
+        else:
+            local_dict[subject]["chain_num"] = max(local_dict[subject]["chain_num"], int(chain_num))
 
-    # add info to correct dictionary
-    if fit_type == 'prebias':
-        local_dict = prebias_subinfo
-    elif fit_type == 'bias':
-        local_dict = bias_subinfo
-    if subject not in local_dict:
-        local_dict[subject] = {"seeds": [], "fit_nums": [], "chain_num": 0}
-    if int(chain_num) == 0:  # if this is the first file of that chain, save some info
-        local_dict[subject]["seeds"].append(seed)
-        local_dict[subject]["fit_nums"].append(fit_num)
+
+    # old code for weeding out old fits (used seeds below 400)
+    for s in prebias_subinfo.keys():
+        if len(prebias_subinfo[s]["fit_nums"]) == 48 or len(prebias_subinfo[s]["fit_nums"]) == 32:
+            new_fit_nums = []
+            new_seeds = []
+            for fit_num, seed in zip(prebias_subinfo[s]["fit_nums"], prebias_subinfo[s]["seeds"]):
+                if int(seed) >= 400:
+                    new_fit_nums.append(fit_num)
+                    new_seeds.append(seed)
+            prebias_subinfo[s]["fit_nums"] = new_fit_nums
+            prebias_subinfo[s]["seeds"] = new_seeds
+
+    # save results
+    json.dump(prebias_subinfo, open(file_prefix + "/canonical_infos_fitvar_{}.json".format(fitvar), 'w'))
+    json.dump(bias_subinfo, open(file_prefix + "/canonical_infos_bias_fitvar_{}.json".format(fitvar), 'w'))
+
+if __name__ == "__main__":
+    fitvar = '0.04'
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--cluster", action="store_true")
+    args = parser.parse_args()
+
+    if args.cluster:
+        file_prefix =  '/usr/src/app'
     else:
-        local_dict[subject]["chain_num"] = max(local_dict[subject]["chain_num"], int(chain_num))
+        file_prefix = '.'  # cluster or local prefix
+    data_folder = "dynamic_GLMiHMM_crossvals"
 
-
-# old code for weeding out old fits (used seeds below 400)
-for s in prebias_subinfo.keys():
-    if len(prebias_subinfo[s]["fit_nums"]) == 48 or len(prebias_subinfo[s]["fit_nums"]) == 32:
-        new_fit_nums = []
-        new_seeds = []
-        for fit_num, seed in zip(prebias_subinfo[s]["fit_nums"], prebias_subinfo[s]["seeds"]):
-            if int(seed) >= 400:
-                new_fit_nums.append(fit_num)
-                new_seeds.append(seed)
-        prebias_subinfo[s]["fit_nums"] = new_fit_nums
-        prebias_subinfo[s]["seeds"] = new_seeds
-
-# save results
-json.dump(prebias_subinfo, open(file_prefix + "/canonical_infos_fitvar_{}.json".format(fitvar), 'w'))
-json.dump(bias_subinfo, open(file_prefix + "/canonical_infos_bias_fitvar_{}.json".format(fitvar), 'w'))
+    index_mice(file_prefix, data_folder, fitvar)
--- raw_fit_processing_part1.py
+++ raw_fit_processing_part1.py
@@ -20,158 +20,180 @@ from mcmc_chain_analysis import state_size_helper, state_num_helper, find_good_c
 import index_mice  # executes function for creating dict of available fits
 from dyn_glm_chain_analysis import MCMC_result_list
 import sys
+import argparse
 
 
-fit_type = ['prebias', 'bias', 'all', 'prebias_plus', 'zoe_style'][0]
-file_prefix = ['.', '/usr/src/app'][0]  # cluster or local prefix
-data_folder = 'dynamic_GLMiHMM_crossvals'
-prethinned = None
 
-subjects = ['DY_013']
-subjects = [subjects[int(sys.argv[1])]]
-
-thinning = 25  # MCMC chain thinning, drop all but every 25th sample
-
-fit_variance = 0.04
-
-# prepare funtions for computing R^hat
-func1 = state_num_helper(0.2)
-func2 = state_num_helper(0.1)
-func3 = state_size_helper()
-func4 = state_size_helper(1)
-dur = 'yes'
-
-# load file which stores info about all fitted animals
-loading_info = json.load(open(file_prefix + "/canonical_infos_fitvar_{}.json".format(fit_variance), 'r'))
-
-def sample_dimensionality_reduction(test, subject, fit_type):
+def sample_dimensionality_reduction(test, subject, file_prefix, fit_type, fit_variance):
     dim = 3 # number of dimensions to reduce to
 
     print('Doing PCA')
-    ev, eig, projection_matrix, xy, z = test.state_pca(subject, dim=dim)#, save_add="_var_{}".format(fit_variance).replace('.', '_'))
+    _, _, _, xy, z = test.state_pca(subject, dim=dim)#, save_add="_var_{}".format(fit_variance).replace('.', '_'))
     pickle.dump((xy, z), open(file_prefix + "/multi_chain_saves/xyz_{}_{}_var_{}.p".format(subject, fit_type, fit_variance), 'wb'))
 
-for subject in subjects:
-    
-    
-    if os.path.isfile(file_prefix + "/multi_chain_saves/" + "canonical_result_{}_prebias_var_0.04.p".format(subject)):
-        print("done already")
-        # quit()
-    assert len(loading_info[subject]["seeds"]) == 16
-    assert len(loading_info[subject]["fit_nums"]) == 16
 
-    r_hats = {}
-    results = []
-    summary_info = {"thinning": thinning, "contains": [], "seeds": [], "fit_nums": []}
-    m = len(loading_info[subject]["fit_nums"])  # number of chains
-    print(subject)
-    n_runs = -1
-    chains1 = None
+def run_subjects(subjects, file_prefix, data_folder, fit_type, fit_variance, prethinned, thinning):
+    # load file which stores info about all fitted animals
+    loading_info = json.load(open(file_prefix + "/canonical_infos_fitvar_{}.json".format(fit_variance), 'r'))
 
-    for j, (seed, fit_num) in enumerate(zip(loading_info[subject]['seeds'], loading_info[subject]['fit_nums'])):
-        print(seed)
-        # info_dict = pickle.load(open(file_prefix + "/session_data/{}_info_dict.p".format(subject), "rb"))
-        samples = []
-
-        mini_counter = 1 # start reading files at 1, discard first 4000 samples (first file) as burnin
-        while True:
-            try:
-                file = file_prefix + "/{}/{}_fittype_{}_var_{}_{}_{}{}.p".format(data_folder, subject, fit_type, fit_variance, seed, fit_num, '_{}'.format(mini_counter))
-                samples += pickle.load(open(file, "rb"))
-                if prethinned is None:
-                    prethinned = len(samples) < 2000  # check whether we already reduced the sample size
-                mini_counter += 1
-            except Exception:
-                print("not found {}".format(file))
-                break
-
-        # make sure that all chains have the same number of files (and therefore samples)
-        if n_runs == -1:
-            n_runs = mini_counter
-        else:
-            if n_runs != mini_counter:
-                print("Problem")
-                print(n_runs, mini_counter)
-                quit()
-
-        print("num of parts per chain: {}".format(mini_counter))
-        save_id = "{}_fittype_{}_var_{}_{}_{}.p".format(subject, fit_type, fit_variance, seed, fit_num).replace('.', '_')
+    # prepare funtions for computing R^hat
+    func1 = state_num_helper(0.2)
+    func2 = state_num_helper(0.1)
+    func3 = state_size_helper()
+    func4 = state_size_helper(1)
+    dur = 'yes'
 
 
-        print("loaded seed {}".format(seed))
+    for subject in subjects:
         
-        if prethinned:
-            # we already thinned the chains
-            samples = samples
-            print(len(samples))
+        if os.path.isfile(file_prefix + "/multi_chain_saves/" + "canonical_result_{}_prebias_var_0.04.p".format(subject)):
+            print("done already")
+            # quit()
+        assert len(loading_info[subject]["seeds"]) == 16
+        assert len(loading_info[subject]["fit_nums"]) == 16
+
+        r_hats = {}
+        results = []
+        summary_info = {"thinning": thinning, "contains": [], "seeds": [], "fit_nums": []}
+        m = len(loading_info[subject]["fit_nums"])  # number of chains
+        print(subject)
+        n_runs = -1
+        chains1 = None
+
+        for j, (seed, fit_num) in enumerate(zip(loading_info[subject]['seeds'], loading_info[subject]['fit_nums'])):
+            print(seed)
+            # info_dict = pickle.load(open(file_prefix + "/session_data/{}_info_dict.p".format(subject), "rb"))
+            samples = []
+
+            mini_counter = 1 # start reading files at 1, discard first 4000 samples (first file) as burnin
+            while True:
+                try:
+                    file = file_prefix + "/{}/{}_fittype_{}_var_{}_{}_{}{}.p".format(data_folder, subject, fit_type, fit_variance, seed, fit_num, '_{}'.format(mini_counter))
+                    samples += pickle.load(open(file, "rb"))
+                    if prethinned is None:
+                        prethinned = len(samples) < 2000  # check whether we already reduced the sample size
+                    mini_counter += 1
+                except Exception:
+                    print("not found {}".format(file))
+                    break
+
+            # make sure that all chains have the same number of files (and therefore samples)
+            if n_runs == -1:
+                n_runs = mini_counter
+            else:
+                if n_runs != mini_counter:
+                    print("Problem")
+                    print(n_runs, mini_counter)
+                    quit()
+
+            print("num of parts per chain: {}".format(mini_counter))
+            save_id = "{}_fittype_{}_var_{}_{}_{}.p".format(subject, fit_type, fit_variance, seed, fit_num).replace('.', '_')
+
+
+            print("loaded seed {}".format(seed))
+
+            if prethinned:
+                # we already thinned the chains
+                samples = samples
+                print(len(samples))
+            else:
+                samples = samples[::thinning]
+            result = MCMC_result(samples,
+                                infos={'subject': subject}, data=pickle.load(open(file_prefix + "/{}/{}_fittype_{}_var_{}_{}_{}{}.p".format(data_folder, subject, fit_type, fit_variance, seed, fit_num, '_{}'.format(0)), "rb"))[0].datas,  # used to pass info_dict
+                                 sessions=fit_type, fit_variance=fit_variance,
+                                 dur=dur, save_id=save_id)
+            results.append(result)
+            print(result.data is None)
+
+            if chains1 is None:
+                n = len(samples)
+                chains1 = np.zeros((m, n))
+                chains2 = np.zeros((m, n))
+                chains3 = np.zeros((m, n))
+                chains4 = np.zeros((m, n))
+
+            res = func1(result)
+            chains1[j] = res
+            res = func2(result)
+            chains2[j] = res
+            res = func3(result)
+            chains3[j] = res
+            res = func4(result)
+            chains4[j] = res
+
+        pickle.dump(chains1, open(file_prefix + "/multi_chain_saves/{}_state_num_0_fittype_{}_var_{}_{}_{}_state_num.p".format(subject, fit_type, fit_variance, seed, fit_num), 'wb'))
+        pickle.dump(chains2, open(file_prefix + "/multi_chain_saves/{}_state_num_1_fittype_{}_var_{}_{}_{}_state_num.p".format(subject, fit_type, fit_variance, seed, fit_num), 'wb'))
+        pickle.dump(chains3, open(file_prefix + "/multi_chain_saves/{}_largest_state_0_fittype_{}_var_{}_{}_{}_state_num.p".format(subject, fit_type, fit_variance, seed, fit_num), 'wb'))
+        pickle.dump(chains4, open(file_prefix + "/multi_chain_saves/{}_largest_state_1_fittype_{}_var_{}_{}_{}_state_num.p".format(subject, fit_type, fit_variance, seed, fit_num), 'wb'))
+
+        # R^hat tests
+        # test = MCMC_result_list([fake_result(100) for i in range(8)])
+        # test.r_hat_and_ess(return_ascending, False)
+        # test.r_hat_and_ess(return_ascending_shuffled, False)
+
+        print()
+        print("Checking R^hat, finding best subset of chains")
+
+        sol, final_r_hat = find_good_chains_unsplit_greedy(chains1, chains2, chains3, chains4, reduce_to=8)
+        r_hats[subject] = final_r_hat
+        loading_info[subject]['ignore'] = sol
+
+        print(r_hats[subject])
+        json.dump(loading_info, open(file_prefix + "/multi_chain_saves/canonical_infos_{}_{}_var_{}.json".format(subject, fit_type, fit_variance), 'w'))
+        json.dump(r_hats, open(file_prefix + "/multi_chain_saves/canonical_info_r_hats_{}_{}_var_{}.json".format(subject, fit_type, fit_variance), 'w'))
+
+        if r_hats[subject] >= 1.05:
+            print("Skipping canonical result due to R^hat")
+            continue
         else:
-            samples = samples[::thinning]
-        result = MCMC_result(samples,
-                            infos={'subject': subject}, data=pickle.load(open(file_prefix + "/{}/{}_fittype_{}_var_{}_{}_{}{}.p".format(data_folder, subject, fit_type, fit_variance, seed, fit_num, '_{}'.format(0)), "rb"))[0].datas,  # used to pass info_dict
-                             sessions=fit_type, fit_variance=fit_variance,
-                             dur=dur, save_id=save_id)
-        results.append(result)
-        print(result.data is None)
+            print("Making canonical result")
 
-        if chains1 is None:
-            n = len(samples)
-            chains1 = np.zeros((m, n))
-            chains2 = np.zeros((m, n))
-            chains3 = np.zeros((m, n))
-            chains4 = np.zeros((m, n))
+        # subset data
+        summary_info['contains'] = [i for i in range(m) if i not in sol]
+        summary_info['seeds'] = [loading_info[subject]['seeds'][i] for i in summary_info['contains']]
+        summary_info['fit_nums'] = [loading_info[subject]['fit_nums'] for i in summary_info['contains']]
+        results = [results[i] for i in summary_info['contains']]
 
-        res = func1(result)
-        chains1[j] = res
-        res = func2(result)
-        chains2[j] = res
-        res = func3(result)
-        chains3[j] = res
-        res = func4(result)
-        chains4[j] = res
-    
-    pickle.dump(chains1, open(file_prefix + "/multi_chain_saves/{}_state_num_0_fittype_{}_var_{}_{}_{}_state_num.p".format(subject, fit_type, fit_variance, seed, fit_num), 'wb'))
-    pickle.dump(chains2, open(file_prefix + "/multi_chain_saves/{}_state_num_1_fittype_{}_var_{}_{}_{}_state_num.p".format(subject, fit_type, fit_variance, seed, fit_num), 'wb'))
-    pickle.dump(chains3, open(file_prefix + "/multi_chain_saves/{}_largest_state_0_fittype_{}_var_{}_{}_{}_state_num.p".format(subject, fit_type, fit_variance, seed, fit_num), 'wb'))
-    pickle.dump(chains4, open(file_prefix + "/multi_chain_saves/{}_largest_state_1_fittype_{}_var_{}_{}_{}_state_num.p".format(subject, fit_type, fit_variance, seed, fit_num), 'wb'))
+        test = MCMC_result_list(results, summary_info)
+        pickle.dump(test, open(file_prefix + "/multi_chain_saves/canonical_result_{}_{}_var_{}.p".format(subject, fit_type, fit_variance), 'wb'))
 
-    # R^hat tests
-    # test = MCMC_result_list([fake_result(100) for i in range(8)])
-    # test.r_hat_and_ess(return_ascending, False)
-    # test.r_hat_and_ess(return_ascending_shuffled, False)
+        test.r_hat_and_ess(state_num_helper(0.2), False)
+        test.r_hat_and_ess(state_num_helper(0.1), False)
+        test.r_hat_and_ess(state_num_helper(0.05), False)
+        test.r_hat_and_ess(state_size_helper(), False)
+        test.r_hat_and_ess(state_size_helper(1), False)
+        test.r_hat_and_ess(gamma_func, True)
+        test.r_hat_and_ess(alpha_func, True)
 
-    print()
-    print("Checking R^hat, finding best subset of chains")
+        print('Computing sub result')
+        sample_dimensionality_reduction(test, subject, fit_type)
 
-    sol, final_r_hat = find_good_chains_unsplit_greedy(chains1, chains2, chains3, chains4, reduce_to=8)
-    r_hats[subject] = final_r_hat
-    loading_info[subject]['ignore'] = sol
 
-    print(r_hats[subject])
-    json.dump(loading_info, open(file_prefix + "/multi_chain_saves/canonical_infos_{}_{}_var_{}.json".format(subject, fit_type, fit_variance), 'w'))
-    json.dump(r_hats, open(file_prefix + "/multi_chain_saves/canonical_info_r_hats_{}_{}_var_{}.json".format(subject, fit_type, fit_variance), 'w'))
+if __name__ == "__main__":
 
-    if r_hats[subject] >= 1.05:
-        print("Skipping canonical result due to R^hat")
-        continue
+    parser = argparse.ArgumentParser()
+
+    fit_types = ['prebias', 'bias', 'all', 'prebias_plus', 'zoe_style']
+    subjects = ['DY_013']
+
+    parser.add_argument("-ft", "--fit_type", type=int, choices=range(len(fit_types)), default=0)
+    parser.add_argument("-c", "--cluster", action="store_true")
+    parser.add_argument("--subject_index", type=int, choices=range(len(subjects)), default=0)
+    args = parser.parse_args()
+
+    fit_type = fit_types[args.fit_type]
+    if args.cluster:
+        file_prefix =  '/usr/src/app'
     else:
-        print("Making canonical result")
+        file_prefix = '.'  # cluster or local prefix
+    data_folder = 'dynamic_GLMiHMM_crossvals'
+    subjects = [subjects[args.subject_index]]
 
-    # subset data
-    summary_info['contains'] = [i for i in range(m) if i not in sol]
-    summary_info['seeds'] = [loading_info[subject]['seeds'][i] for i in summary_info['contains']]
-    summary_info['fit_nums'] = [loading_info[subject]['fit_nums'] for i in summary_info['contains']]
-    results = [results[i] for i in summary_info['contains']]
+    prethinned = None
+    thinning = 25  # MCMC chain thinning, drop all but every 25th sample
 
-    test = MCMC_result_list(results, summary_info)
-    pickle.dump(test, open(file_prefix + "/multi_chain_saves/canonical_result_{}_{}_var_{}.p".format(subject, fit_type, fit_variance), 'wb'))
+    fit_variance = 0.04
 
-    test.r_hat_and_ess(state_num_helper(0.2), False)
-    test.r_hat_and_ess(state_num_helper(0.1), False)
-    test.r_hat_and_ess(state_num_helper(0.05), False)
-    test.r_hat_and_ess(state_size_helper(), False)
-    test.r_hat_and_ess(state_size_helper(1), False)
-    test.r_hat_and_ess(gamma_func, True)
-    test.r_hat_and_ess(alpha_func, True)
+    index_mice.index_mice(file_prefix, data_folder, fit_variance)
 
-    print('Computing sub result')
-    sample_dimensionality_reduction(test, subject, fit_type)
+    run_subjects(subjects, file_prefix, data_folder, fit_type, fit_variance, prethinned, thinning)
